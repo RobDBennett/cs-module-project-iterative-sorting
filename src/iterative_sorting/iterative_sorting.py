adbde08e1dc64c4d9be9789dc8fbc178ceb521e8
RobDBennett <robbennett.work@gmail.com>
2020-09-14 12:03:06 -0700

    Day 1 + stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,20 +6,22 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
-
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
         # TO-DO: swap
-        # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 '''
@@ -40,7 +42,72 @@ buckets.
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
-
-
+    if maximum == None and len(arr) != 0:
+        maximum = max(arr)
+    else:
+        return []
+  
+    n = len(arr)
+    m = maximum + 1
+    count = [0] * m
+    for a in arr:
+        if a >= 0:
+            count[a] += 1
+        else:
+            return "Error, negative numbers not allowed in Count Sort"
+    
+    i = 0
+    for a in range(m):
+        for c in range(count[a]):
+            arr[i] = a
+            i += 1
+    
     return arr
+
+
+
+
+
+
+#    if maximum == None:
+#        maximum = len(arr)
+#    counts = [0] * ((maximum) + 1)
+#    for item in arr:
+#        if item >= 0:
+#            counts[item] += 1
+#        else:
+#            return "Error, negative numbers not allowed in Count Sort"
+#    
+#    num_items_before = 0
+#    for i, count in enumerate(counts):
+#        counts[i] = num_items_before
+#        num_items_before += count
+#    
+#    sorted_list = [None] * len(arr)
+#    for item in arr:
+#        sorted_list[ counts[item] ] = item
+#        counts[item] += 1
+#    return sorted_list
+
+#    size = len(arr)
+#    output = [0] * size
+#    count = [0] * 10
+#    for i in range(0, size):
+#        count[arr[i]] += 1
+#    
+#    for i in range(1, 10):
+#        count[i] += count[i-1]
+#    
+#    i = size - 1
+#    while i >= 0:
+#        if arr[i] >= 0:
+#            output[count[arr[i]] - 1 ] = arr[i]
+#            count[arr[i]] -= 1
+#            i -= 1
+#        else:
+#            return "Error, negative numbers not allowed in Count Sort"
+#    
+#    for i in range(0, size):
+#        arr[i] = output[i]
+#
+#    return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        midpoint = (right + left) // 2
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] < target:
+            #Get rid of everything on the left.
+            left = midpoint + 1
+        else:
+            right = midpoint - 1
 
 
     return -1  # not found
