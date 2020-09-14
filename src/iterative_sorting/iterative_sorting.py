b3aa854c458a1b5ff3426502cec51599c5d3b36d
RobDBennett <robbennett.work@gmail.com>
2020-09-14 13:30:38 -0700

    Day 1 stretch + documentation.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -24,6 +24,7 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+
 '''
 STRETCH: implement the Counting Sort function below
 
@@ -41,32 +42,44 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
+    """
+    This function executes a counting sort. The first three
+    if statements are for testing specific circumstances.
+    maximum represents the highest value in the array. If this value
+    is provided, we operate with it. Otherwise we execute a max() on the 
+    of the given array to find it.
+    We then create an array of 0's equal to the max value +1. Then a 
+    for loop is executed which creates 'buckets' of each count of values.
+    We instantiate an index count, and then execute a nested for loop that
+    goes through a range equal to the max+1, and then through the range of the 
+    each item of the count. 
+    Finally, the array index is altered to place the numbers properly, 
+    and the index counter is increased.
+    """
     if maximum == None and len(arr) != 0:
-        maximum = max(arr)
-    else:
-        return []
-  
-    n = len(arr)
-    m = maximum + 1
+        maximum = max(arr)  # Alters Maximum away from default None.
+    if arr == []:
+        return arr  # Checks if the array is empty before sorting.
+    if min(arr) < 0:  # Checks that each item is positive.
+        return "Error, negative numbers not allowed in Count Sort"
+
+    m = maximum + 1  # Add a value to ensure python gets to the proper number.
+    # Create a list of 0s equal to the maximum value +1 (to account for 0)
     count = [0] * m
-    for a in arr:
-        if a >= 0:
-            count[a] += 1
-        else:
-            return "Error, negative numbers not allowed in Count Sort"
-    
-    i = 0
-    for a in range(m):
-        for c in range(count[a]):
+    for a in arr:  # Loop through the array, adding to count index each time that value appears.
+        count[a] += 1
+
+    i = 0  # Instantiate a separate counter.
+    for a in range(m):  # Loop through the range of 0 to the max +1
+        for c in range(count[a]):  # Loop through the items in the count array
+            # Replaces the starting array element by element so that the order reflects the index counter and array value.
             arr[i] = a
             i += 1
-    
+
     return arr
-
-
-
-
 
 
 #    if maximum == None:
@@ -77,12 +90,12 @@ def counting_sort(arr, maximum=None):
 #            counts[item] += 1
 #        else:
 #            return "Error, negative numbers not allowed in Count Sort"
-#    
+#
 #    num_items_before = 0
 #    for i, count in enumerate(counts):
 #        counts[i] = num_items_before
 #        num_items_before += count
-#    
+#
 #    sorted_list = [None] * len(arr)
 #    for item in arr:
 #        sorted_list[ counts[item] ] = item
@@ -94,10 +107,10 @@ def counting_sort(arr, maximum=None):
 #    count = [0] * 10
 #    for i in range(0, size):
 #        count[arr[i]] += 1
-#    
+#
 #    for i in range(1, 10):
 #        count[i] += count[i-1]
-#    
+#
 #    i = size - 1
 #    while i >= 0:
 #        if arr[i] >= 0:
@@ -106,7 +119,7 @@ def counting_sort(arr, maximum=None):
 #            i -= 1
 #        else:
 #            return "Error, negative numbers not allowed in Count Sort"
-#    
+#
 #    for i in range(0, size):
 #        arr[i] = output[i]
 #
